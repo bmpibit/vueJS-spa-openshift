1547207130b0381a00acd696334e9c7a55812035
unknown <massimo.bergalla@pianobit.com>
2019-06-28 12:13:43 +0200

    express ip and host variables

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express(); 
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 // serves files from the root directory
 app.use(express.static('./'));
-app.listen(8080, function () {    
+app.listen(port, ip, function () {    
   console.log('Listening at http://localhost:8080');  
 });
